65233867df9090e0d457ce92c7c113ffd2be34f0
chimi555 <chinami.s515@gmail.com>
2020-01-04 01:00:52 +0900

    deviseでログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/signup', to: 'users#new'
+  devise_for :users, controllers: {
+  confirmations: 'users/confirmations',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations',
+  sessions:      'users/sessions',
+  }
 end
