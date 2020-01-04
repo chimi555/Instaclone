8092dd57bc0e55fe60be17830a2e51ee8080bf1b
chimi555 <chinami.s515@gmail.com>
2020-01-04 23:13:48 +0900

    deviseでuser_nameかemailのどちらかでもログインできるよう変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  #def configure_permitted_parameters
+    #devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
+    #devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
+  #end
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
+    added_attrs = [:name, :user_name, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_accessor :login
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable
+         :recoverable, :rememberable, :validatable, :omniauthable,
+         authentication_keys: [:login]
 
   validates :name, presence: true
-  validates :user_name, presence: true
+  validates :user_name, presence: true, uniqueness: { case_sensitive: false }
 
 
   def self.find_for_oauth(auth)
@@ -26,5 +28,20 @@ class User < ApplicationRecord
 
     user
   end
-  
+
+  #
+  def login
+    @login || self.user_name || self.email
+  end
+
+  #ログイン認証の条件をオーバーライド
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(user_name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:user_name) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
 end
