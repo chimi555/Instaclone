91619a758a067b69372b4994793815110f752fa2
chimi555 <chinami.s515@gmail.com>
2020-01-07 20:23:53 +0900

    画像のバリデーション追加

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,15 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
+  validates :picture, presence: true
+  validates :picture_size
+
+  private
+
+    #アップロードされた画像のサイズをバリデーションする
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "shoule be less than 5MB")
+      end
+    end
 end
