5a6ef38b7a156440500abf6f1e26c884f2e46f05
chimi555 <chinami.s515@gmail.com>
2020-01-09 19:37:53 +0900

    followingページ、followersページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
-
   end
 
   def index
     @users = User.paginate(page: params[:page])
+  end
+
+  def following
+    @title = "フォロー中"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "フォロワー"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 end
