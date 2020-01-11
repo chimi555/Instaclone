8133cd6898fff132348ffd0dfb048f60f804fd9a
chimi555 <chinami.s515@gmail.com>
2020-01-11 17:00:08 +0900

    いいねボタンAjax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,15 +2,29 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    micropost = Micropost.find(params[:micropost_id])
-    current_user.like(micropost)
-    redirect_to root_url
+    @micropost = Micropost.find(params[:micropost_id])
+    unless current_user.already_liked?(@micropost)
+      current_user.like(@micropost)
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
+      end
+    else
+      redirect_to request.referrer || root_url
+    end
   end
 
   def destroy
-    micropost = Like.find(params[:id]).micropost
-    current_user.unlike(micropost)
-    redirect_to root_url
+    @micropost = Like.find(params[:id]).micropost
+    if current_user.already_liked?(@micropost)
+      current_user.unlike(@micropost)
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
+      end
+    else
+      redirect_to request.referrer || root_url
+    end
   end
 
 end
